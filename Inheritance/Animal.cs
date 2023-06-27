9172635a22b3425543cade92e6b40a756e02df49
Johnathon Melton <seventh.acoustic@gmail.com>
2023-06-26 22:57:41 -0500

    Inheritance Exercise

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Animal
+    {
+        public Animal()
+        {
+
+        }
+        public int Age { get; set; }
+        public int LegCount { get; set; }
+        public bool isAlive { get; set; }
+        public string LandSeaAir { get; set; }
+
+    }
+}
---
+++ Inheritance/Bird.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Bird : Animal
+    {
+        public Bird()
+        {
+            isAlive = true;
+            LegCount = 2;
+            LandSeaAir = "Air";
+            Age = 0;
+        }
+        public string WingColor { get; set; }
+        public bool CanFly { get; set; }
+        public bool WillMigrate { get; set; }
+        public double BeakLength { get; set; }
+
+    }   
+}
